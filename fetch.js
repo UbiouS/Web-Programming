2d4868f0f6ff39c93a3aa2b72f79a2e44a987c76
UbiouS <irakli1472583690@gmail.com>
2021-04-13 19:33:50 +0300

    Completing 2 and starting 3 labs

--- fetch.js
+++ fetch.js
@@ -1,12 +1,9 @@
 class Fetch {
   async getCurrent(input) {
-    const myKey = "39a9a737b07b4b703e3d1cd1e231eedc";
 
-    //make request to url
+    const key = "39a9a737b07b4b703e3d1cd1e231eedc";
 
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}`
-    );
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${key}`);
 
     const data = await response.json();
 
--- ui.js
+++ ui.js
@@ -10,15 +10,43 @@ class UI {
 
     //add them to inner HTML
 
-    this.uiContainer.innerHTML = `
-        
-        <div class="card mx-auto mt-5" style="width: 18rem;">
-            <div class="card-body justify-content-center">
-                <h5 class="card-title">${data.name}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Highs of ${data.main.temp_max}. Lows of ${data.main.temp_min}</h6>
-                <p class="card-text ">Weather conditions are described as: ${data.weather[0].description}</p>
-                
+    this.uiContainer.innerHTML = `     
+            <div class="secondary-city">
+            <div class="name-of-secondary-block">
+                <h4 class="card-title">${data.name}</h4>
+                <div class="secondary-temperature">
+                    ${data.main.temp  - 273,15}°C
+                </div>
+                <div class="secondary-icon-display">
+                    <img class="weather-icon" src="svg/038-foggy-2.svg">
+                </div>
+                <div class="exit-buttons">
+                    <button class="close-button">ͯ</button>
+                </div>
             </div>
+
+            <ul>
+                <li>
+                    <div class="inside-text">Ветер</div>
+                    <div class="wind-and-stuff"> Moderate breeze, 6.0 m/s, North-northwest</div>
+                </li>
+                <li>
+                    <div class="inside-text">Облачность</div>
+                    <div class="wind-and-stuff">${data.weather[0].description}</div>
+                </li>
+                <li>
+                    <div class="inside-text">Давление</div>
+                    <div class="wind-and-stuff">${data.main.pressure} hpa</div>
+                </li>
+                <li>
+                    <div class="inside-text">Влажность</div>
+                    <div class="wind-and-stuff"> ${data.main.humidity}%</div>
+                </li>
+                <li>
+                    <div class="inside-text">Координаты</div>
+                    <div class="wind-and-stuff">[${data.coord.lat}, ${data.coord.lon}]</div>
+                </li>
+            </ul>
         </div>
         
         
